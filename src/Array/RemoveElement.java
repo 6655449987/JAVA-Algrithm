91c68cdf92db3089b295d9124a5e2cd5d84e47bf
6655449987 <6655449987qq@gmail.com>
2015-04-04 10:38:21 +0800

    RemoveElement

---
+++ src/Array/RemoveElement.java
@@ -0,0 +1,46 @@
+package Array;
+
+/**
+ * Given an array and a value, remove all instances of that value in place and return the new length.
+ * 
+ * @author shengyuan
+ *
+ */
+public class RemoveElement {
+
+	public static void main(String[] args) {
+		RemoveElement o = new RemoveElement();
+		
+		int[] A = new int[] {1,2,3,4,4,5,6,7,5,6,7,8};
+		int ele = 5;
+		
+		System.out.println(String.format("remove %s from %s, and return : %s \rand now A becomes : %s", o.toString(A), ele, o.removeElement(A, ele), o.toString(A)));
+		
+	}
+
+	public int removeElement(int[] A, int elem) {
+		int m = 0;
+		for (int i = 0; i < A.length; i++) {
+
+			if (A[i] != elem) {
+				A[m] = A[i];
+				m++;
+			}
+		}
+
+		return m;
+	}
+	
+	
+	public String toString(int[] A){
+		StringBuilder sb = new StringBuilder("{");
+		for(int o : A){
+			sb.append(o).append(",");
+		}
+		String rs = sb.substring(0, sb.length()-1);
+		rs+="}";
+		return rs;
+		
+		
+	}
+}
